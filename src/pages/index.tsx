eb3a7b6f56d2928f23b6634664646b0fdbbd092f
Ayush Seth <ayush-seth@outlook.com>
2023-04-27 15:34:11 +0530

    added main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,91 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Input } from "@/components/input";
+import { TimeSpan } from "@/components/timespan";
+import { calculateAge } from "@/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700", "800"] });
+
+const DateFormSchema = z.object({
+  day: z.coerce
+    .number()
+    .gt(0, { message: "This field is required" })
+    .gte(1, { message: "Must be a valid day" })
+    .lte(31, { message: "Must be a valid day" }),
+  month: z.coerce
+    .number()
+    .gt(0, { message: "This field is required" })
+    .gte(1, { message: "Must be a valid month" })
+    .lte(12, { message: "Must be a valid month" }),
+  year: z.coerce
+    .number({
+      invalid_type_error: "This field is required",
+      required_error: "This field is required",
+    })
+    .gt(0, { message: "This field is required" })
+    .lte(new Date().getFullYear(), { message: "Must be in the past" }),
+});
+
+export type DateForm = z.infer<typeof DateFormSchema>;
 
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<DateForm>({
+    resolver: zodResolver(DateFormSchema),
+  });
+  const [age, setAge] = useState<DateForm>();
+
+  const onSubmit: SubmitHandler<DateForm> = ({ day, month, year }) => {
+    setAge(calculateAge({ day, month, year }));
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    <div
+      className={`${poppins.className} grid h-screen w-screen place-items-center bg-neutral-200`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="m-4 rounded-2xl rounded-br-[100px] bg-white p-8 sm:w-[550px]">
+        <form className="flex gap-4 sm:gap-6">
+          <Input
+            label="day"
+            placeholder="DD"
+            {...register("day")}
+            error={errors.day?.message}
+          />
+          <Input
+            label="month"
+            placeholder="MM"
+            {...register("month")}
+            error={errors.month?.message}
+          />
+          <Input
+            label="year"
+            placeholder="YYYY"
+            {...register("year")}
+            error={errors.year?.message}
+          />
+        </form>
+        <div className="relative my-14 sm:my-6">
+          <div className="h-px grow bg-slate-200" />
+          <button
+            className="absolute -top-8 right-[calc(50%-28px)] grid h-16 w-16 place-items-center rounded-full bg-purple-600 hover:bg-black sm:-top-6 sm:right-0 sm:h-14 sm:w-14"
+            onClick={handleSubmit(onSubmit)}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <Image src="/icon-arrow.svg" width={24} height={24} alt="" />
+          </button>
+        </div>
+        <div className="flex flex-col gap-2">
+          <TimeSpan value={age?.year} unit="years" />
+          <TimeSpan value={age?.month} unit="months" />
+          <TimeSpan value={age?.day} unit="days" />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
